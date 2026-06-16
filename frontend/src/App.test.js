d374855ded50562d35e95e3e834401b8cda2e7d9
sarahmahmoudi315 <mahmoudi.sarra@esprit.tn>
2026-06-16 13:58:44 +0200

    add unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('la barre de recherche est affichée', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const searchInput = screen.getByPlaceholderText(/rechercher/i);
+  expect(searchInput).toBeInTheDocument();
+});
+
+test('le titre Film App est affiché', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const title = screen.getByText(/Film App/i);
+  expect(title).toBeInTheDocument();
 });
